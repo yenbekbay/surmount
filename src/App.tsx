52478a19fe5dc30541844a12bf8d40bec135f584
Ayan Yenbekbay <ayan.yenb@gmail.com>
2019-06-21 10:05:46 +0200

    Set up Tailwind css framework

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface AppProps {}
 
 export const App: React.FC<AppProps> = _props => (
-  <div>
-    <h1>Hello World!</h1>
+  <div className="min-h-screen flex items-center justify-center">
+    <h1 className="text-5xl text-purple font-sans">Hello world!</h1>
   </div>
 );
--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,3 +1,11 @@
+// node environment
+declare const require: (id: string) => any;
+declare const process: {
+  env: {
+    [key: string]: string | undefined;
+  };
+};
+
 // HMR
 declare const module: {
   hot?: {
